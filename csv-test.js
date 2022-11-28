a8dc810bd43f600b6c8de7efe1bba75de425ded1
shraddha38 <shraddhasharmapro@gmail.com>
2022-11-27 23:08:52 -0600

    feat:test problem

---
+++ csv-test.js
@@ -0,0 +1,22 @@
+var csv = 'Name,PrevMonthBalance,CurrentMonthBalance,CreditCardBillAmt\n'
+    + 'John,100,200,100\n'
+    + 'Jane,200,300,100\n'
+    + 'Joe,300,400,100\n';
+function csvToJson(keys, types, values) {
+    return keys.reduce(function (acc, curr, currIndex) {
+        var currValue = types[currIndex] === 'number' ? parseInt(values[currIndex], 10) : values[currIndex];
+        acc[curr] = currValue;
+        return acc;
+    }, {});
+}
+function getMaxFromColumnName(columnName) {
+    var data = csv.split("\n");
+    var header = data.shift().split(',');
+    data.pop();
+    var types = ['string', 'number', 'number', 'number'];
+    var arrayOfObjects = data.map(function (datum) { return csvToJson(header, types, datum.split(',')); });
+    arrayOfObjects.map(function (i) {
+        return Object.keys(i) === columnName ? Math.max(i) : "Shraddha";
+    });
+}
+console.log(getMaxFromColumnName("PrevMonthBalance"));
